c4dcc18f6e01c1a417aa429f13d180ff2b4b57f8
Hanke Jakob <Jakob.HANKE@htlvb.at>
2023-12-14 08:33:34 +0100

    Registrierung

---
+++ Fahrgemeinschaft/Models/RegistrationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Fahrgemeinschaft.Models;
+
+public class RegistrationViewModel
+{
+    
+        [Display(Name = "Vorname")]
+        [Required(ErrorMessage = "Gib bitte deinen Vornamen ein.")]
+        public string? FirstName { get; set; }
+
+        [Display(Name = "Nachname")]
+        [Required(ErrorMessage = "Gib bitte deinen Nachnamen ein.")]
+        public string? LastName { get; set; }
+
+        [Display(Name = "E-Mail")]
+        [Required(ErrorMessage = "Gib bitte deine E-Mail ein.")]
+        public string? MailAddress { get; set; }
+
+        [Display(Name = "Geburtsdatum")]
+        [Required(ErrorMessage = "Gib bitte dein Geburtsdatum ein.")]
+        [DataType(DataType.Date)]
+        public string? BirthDate { get; set; }
+
+        [Display(Name = "Passwort")]
+        [Required(ErrorMessage = "Gib bitte dein Passwort ein.")]
+        public string? Password { get; set; }
+
+        
+    }
